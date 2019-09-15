fb405a3f527f808713594e77f2e1f0c834535946
rclik <rahmancelik@gmail.com>
2019-09-15 18:14:01 +0300

    remaining web services are added and some code parts are changed.

--- maven_project_archive_layerdarcrestapi/src/main/java/com/rcelik/layerdarcrestapi/service/EmployeeService.java
+++ maven_project_archive_layerdarcrestapi/src/main/java/com/rcelik/layerdarcrestapi/service/EmployeeService.java
@@ -5,13 +5,30 @@ import com.rcelik.layerdarcrestapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
+    private final EmployeeRepository repository;
 
     @Autowired
-    private EmployeeRepository repository;
+    public EmployeeService(EmployeeRepository injectedRepository) {
+        this.repository = injectedRepository;
+    }
 
     public Iterable<Employee> getAllEmployees() {
         return repository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public void deleteEmployeeById(Long id) {
+        repository.deleteById(id);
     }
 }
